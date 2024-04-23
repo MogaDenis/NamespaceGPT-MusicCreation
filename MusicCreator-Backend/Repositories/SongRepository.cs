50d641e8dcdb3739b7ed66bbaf0d43b1ea0abf0d
925-Mihai-Matei <teo28042003@gmail.com>
2024-04-24 00:15:57 +0300

    Created song repository tests, song repo warnings solved

--- MusicCreator-Backend/Repositories/SongRepository.cs
+++ MusicCreator-Backend/Repositories/SongRepository.cs
@@ -5,18 +5,30 @@
     using Music.MusicDomain;
     using MusicCreator.Repository.Interfaces;
 
+    /// <summary>
+    ///     Class responsible from getting and writing Song entities to the database.
+    /// </summary>
     public class SongRepository : ISongRepository
     {
-        private readonly IConnectionFactory _connectionFactory;
+        private readonly IConnectionFactory connectionFactory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SongRepository"/> class.
+        /// </summary>
+        /// <param name="connectionFactory"> The connection factory used to create database connections.</param>
         public SongRepository(IConnectionFactory connectionFactory)
         {
-            _connectionFactory = connectionFactory;
+            this.connectionFactory = connectionFactory;
         }
 
+        /// <summary>
+        ///     Adds a new song to the repository.
+        /// </summary>
+        /// <param name="elem">The song to add.</param>
+        /// <returns>The id of the added song.</returns>
         public int Add(Song elem)
         {
-            using SqlConnection connection = this._connectionFactory.GetConnection();
+            using SqlConnection connection = this.connectionFactory.GetConnection();
             connection.Open();
 
             SqlCommand command = connection.CreateCommand();
@@ -31,9 +43,13 @@
             return newSongId;
         }
 
+        /// <summary>
+        ///     Deletes the song with ID from the repository.
+        /// </summary>
+        /// <param name="id">The ID of the song to delete.</param>
         public void Delete(int id)
         {
-            using SqlConnection connection = this._connectionFactory.GetConnection();
+            using SqlConnection connection = this.connectionFactory.GetConnection();
             connection.Open();
 
             SqlCommand command = connection.CreateCommand();
@@ -45,9 +61,14 @@
             command.ExecuteNonQuery();
         }
 
+        /// <summary>
+        ///     Searches for a song with the specified ID in the repository.
+        /// </summary>
+        /// <param name="id">The ID of the song to search for.</param>
+        /// <returns>Thesong with the specified ID, or null if not found.</returns>
         public Song? Search(int id)
         {
-            using SqlConnection connection = this._connectionFactory.GetConnection();
+            using SqlConnection connection = this.connectionFactory.GetConnection();
             connection.Open();
 
             SqlCommand command = connection.CreateCommand();
@@ -64,7 +85,7 @@
                 byte[] byteArray = new byte[byteLength];
                 reader.GetBytes(3, 0, byteArray, 0, (int)byteLength);
 
-                Song song = new(reader.GetInt32(0), reader.GetString(1), 0, byteArray, reader.GetString(2));
+                Song song = new (reader.GetInt32(0), reader.GetString(1), 0, byteArray, reader.GetString(2));
 
                 return song;
             }
@@ -72,9 +93,13 @@
             return null;
         }
 
+        /// <summary>
+        ///     Retrieves all songs from the repository.
+        /// </summary>
+        /// <returns>A list of all songs from the repository.</returns>
         public List<Song> GetAll()
         {
-            using SqlConnection connection = this._connectionFactory.GetConnection();
+            using SqlConnection connection = this.connectionFactory.GetConnection();
             connection.Open();
 
             SqlCommand command = connection.CreateCommand();
