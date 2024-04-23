d8e3d895e7c29974784925692959daaa65d9d5fe
915-Mocanu-Dragos <115075236+915-Mocanu-Dragos@users.noreply.github.com>
2024-04-23 18:07:47 +0300

    update musictagrepo

--- MusicCreator-Backend/Repositories/MusigTagRepository.cs
+++ MusicCreator-Backend/Repositories/MusigTagRepository.cs
@@ -1,65 +1,79 @@
-﻿using System.Data;
-using Microsoft.Data.SqlClient;
-using Music.MusicDomain;
-using MusicCreator.Repository.Interfaces;
+﻿// <copyright file="MusigTagRepository.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace MusicCreator.Repository
 {
+    using System.Data;
+    using Microsoft.Data.SqlClient;
+    using Music.MusicDomain;
+    using MusicCreator.Repository.Interfaces;
+
+    /// <summary>
+    /// .
+    /// </summary>
     public class MusigTagRepository : IMusicTagRepository
     {
-        private readonly SqlConnection _connection;
-        private readonly SqlDataAdapter _adapter;
-        private readonly DataSet _dataset;
-        private readonly DataTable? _table;
+        private readonly SqlConnection connection;
+        private readonly SqlDataAdapter adapter;
+        private readonly DataSet dataset;
+        private readonly DataTable? table;
 
-        private readonly IConnectionFactory _connectionFactory;
+        private readonly IConnectionFactory connectionFactory;
 
-        private static MusicTag GenerateMusicTagFromRowObject(DataRow row)
-        {
-            int id = (int)row["musictag_id"]; // I hope you will do a better job
-            string title = (string)row["tag"];
-            return new MusicTag(id, title);
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MusigTagRepository"/> class.
+        /// </summary>
+        /// <param name="connectionFactory">The connection factory used to create database connections.</param>
         public MusigTagRepository(IConnectionFactory connectionFactory)
         {
-            _connectionFactory = connectionFactory;
+            this.connectionFactory = connectionFactory;
 
             string query = "select * from MUSICTAG";
-            _connection = _connectionFactory.GetConnection();
+            this.connection = this.connectionFactory.GetConnection();
 
-            _adapter = new SqlDataAdapter(query, _connection);
-            _dataset = new DataSet();
+            this.adapter = new SqlDataAdapter(query, this.connection);
+            this.dataset = new DataSet();
 
-            _adapter.Fill(_dataset, "MusicTag");
-            _table = _dataset.Tables["MusicTag"];
+            this.adapter.Fill(this.dataset, "MusicTag");
+            this.table = this.dataset.Tables["MusicTag"];
 
-            var commandBuilder = new SqlCommandBuilder(_adapter);
-            _adapter.InsertCommand = commandBuilder.GetInsertCommand();
+            var commandBuilder = new SqlCommandBuilder(this.adapter);
+            this.adapter.InsertCommand = commandBuilder.GetInsertCommand();
         }
 
+        /// <summary>
+        /// Adds a new music tag to the repository.
+        /// </summary>
+        /// <param name="elem">The music tag to add.</param>
         public void Add(MusicTag elem)
         {
-            if (_table == null)
+            if (this.table == null)
             {
                 return;
             }
 
-            DataRow row = _table.NewRow();
-            //row["musictag_id"] = elem.getId();
+            DataRow row = this.table.NewRow();
+
+            // row["musictag_id"] = elem.getId();
             row["tag"] = elem.Title;
-            _table.Rows.Add(row);
-            _adapter.Update(_dataset, "MusicTag");
+            this.table.Rows.Add(row);
+            this.adapter.Update(this.dataset, "MusicTag");
         }
 
+        /// <summary>
+        /// Searches for a music tag with the specified ID in the repository.
+        /// </summary>
+        /// <param name="id">The ID of the music tag to search for.</param>
+        /// <returns>The music tag with the specified ID, or null if not found.</returns>
         public MusicTag? Search(int id)
         {
-            if (_table == null)
+            if (this.table == null)
             {
                 return null;
             }
 
-            var elems = from DataRow row in _table.Rows
+            var elems = from DataRow row in this.table.Rows
                         where (int)row["musictag_id"] == id
                         select row;
 
@@ -77,17 +91,28 @@ namespace MusicCreator.Repository
             return GenerateMusicTagFromRowObject(elem);
         }
 
+        /// <summary>
+        /// Retrieves all music tags from the repository.
+        /// </summary>
+        /// <returns>A list of all music tags in the repository.</returns>
         public List<MusicTag> GetAll()
         {
-            if (_table == null)
+            if (this.table == null)
             {
-                return [];
+                return new List<MusicTag>();
             }
 
-            var elems = from DataRow row in _table.Rows
+            var elems = from DataRow row in this.table.Rows
                         select GenerateMusicTagFromRowObject(row);
 
             return elems.ToList();
+        }
+
+        private static MusicTag GenerateMusicTagFromRowObject(DataRow row)
+        {
+            int id = (int)row["musictag_id"]; // I hope you will do a better job
+            string title = (string)row["tag"];
+            return new MusicTag(id, title);
         }
     }
 }
